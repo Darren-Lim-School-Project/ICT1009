b794ba825736236e689a0c3ccf9f09badbb71a9f
senkawa <mao@amatsuka.me>
2021-01-31 10:37:59 +0800

    Example mapping

--- src/main/java/moe/ksmz/rodentraid/Api/Controller/IndexController.java
+++ src/main/java/moe/ksmz/rodentraid/Api/Controller/IndexController.java
@@ -1,3 +1,15 @@
 package moe.ksmz.rodentraid.Api.Controller;
 
-public class IndexController {}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+    @GetMapping("/xd")
+    String xd() {
+        return "xd";
+    }
+}
