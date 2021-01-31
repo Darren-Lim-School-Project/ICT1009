34845b462da4fbf3ebf313b0c4ff034b32534204
senkawa <mao@amatsuka.me>
2021-02-01 00:17:50 +0800

    Location stubs

--- src/main/java/moe/ksmz/rodentraid/sck/Domain/Location.java
+++ src/main/java/moe/ksmz/rodentraid/sck/Domain/Location.java
@@ -1,3 +1,17 @@
 package moe.ksmz.rodentraid.sck.Domain;
 
-public class Location {}
+import java.util.List;
+import java.util.Set;
+
+public class Location {
+    private String name;
+    private Set<Mice> mice;
+
+    public boolean hasMice(Mice mice) {
+        return this.mice.contains(mice);
+    }
+
+    public boolean hasMice(List<Mice> mice) {
+        return this.mice.containsAll(mice);
+    }
+}
