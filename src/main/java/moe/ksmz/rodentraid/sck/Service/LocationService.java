cdb067d8b2204871684a22187bd044751090729a
senkawa <mao@amatsuka.me>
2021-02-01 10:44:54 +0800

    Methods for location

--- src/main/java/moe/ksmz/rodentraid/sck/Service/LocationService.java
+++ src/main/java/moe/ksmz/rodentraid/sck/Service/LocationService.java
@@ -2,6 +2,7 @@ package moe.ksmz.rodentraid.sck.Service;
 
 import java.util.List;
 import moe.ksmz.rodentraid.sck.Domain.Location;
+import moe.ksmz.rodentraid.sck.Domain.Mice;
 import moe.ksmz.rodentraid.sck.Service.Contracts.LocationManager;
 
 public class LocationService implements LocationManager {
@@ -10,6 +11,16 @@ public class LocationService implements LocationManager {
     @Override
     public List<Location> loadEntries() {
         loaded = true;
+        return null;
+    }
+
+    @Override
+    public List<Location> canBeFoundIn(Mice mice) {
+        return null;
+    }
+
+    @Override
+    public List<Location> allMiceIn(Location location) {
         return null;
     }
 }
