27ab25d0f21108d0ab4574174b5200e5f8335580
senkawa <mao@amatsuka.me>
2021-02-08 23:11:13 +0800

    Half bonus and luck from bases

--- src/main/java/moe/ksmz/rodentraid/sck/Domain/Base.java
+++ src/main/java/moe/ksmz/rodentraid/sck/Domain/Base.java
@@ -4,4 +4,14 @@ public class Base extends AbstractTrap {
     public Base(String name, Long power, Long bonus, Long luck, Integer freshness) {
         super(name, power, bonus, luck, freshness);
     }
+
+    @Override
+    public Long getBonus() {
+        return bonus / 2;
+    }
+
+    @Override
+    public Long getLuck() {
+        return luck / 2;
+    }
 }
