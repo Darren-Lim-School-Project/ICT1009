982e39539eb82cc8f46a6f0a0ca1ef15a1bef355
senkawa <mao@amatsuka.me>
2021-02-13 01:54:52 +0800

    Static factory

--- src/main/java/moe/ksmz/rodentraid/Response/Auth/UserResponse.java
+++ src/main/java/moe/ksmz/rodentraid/Response/Auth/UserResponse.java
@@ -2,6 +2,7 @@ package moe.ksmz.rodentraid.Response.Auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import moe.ksmz.rodentraid.Models.User;
 import moe.ksmz.rodentraid.sck.Domain.Rank;
 
 @Getter
@@ -10,4 +11,8 @@ public class UserResponse {
     private final String name;
     private final String email;
     private final Rank rank;
+
+    public static UserResponse fromUser(User user) {
+        return new UserResponse(user.getName(), user.getEmail(), user.getCurrentRank());
+    }
 }
