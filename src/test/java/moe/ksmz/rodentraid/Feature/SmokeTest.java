9f851032e81282512386c1ccd7e6e3e925414968
senkawa <mao@amatsuka.me>
2021-02-12 19:28:03 +0800

    Mock session

--- src/test/java/moe/ksmz/rodentraid/Feature/SmokeTest.java
+++ src/test/java/moe/ksmz/rodentraid/Feature/SmokeTest.java
@@ -3,14 +3,23 @@ package moe.ksmz.rodentraid.Feature;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import moe.ksmz.rodentraid.Auth.AuthStatus;
+import moe.ksmz.rodentraid.Models.Repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
+@SpringJUnitWebConfig
 public class SmokeTest {
-    @Autowired private MockMvc http;
+    @Autowired MockMvc http;
+    @Autowired MockHttpSession session;
+    @MockBean AuthStatus authStatus;
+    @MockBean UserRepository userRepository;
 
     @Test
     void itWorks() throws Exception {
