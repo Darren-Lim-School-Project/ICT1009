e4dec77094ac4e01a1502cd82f60333742a73fbb
senkawa <mao@amatsuka.me>
2021-02-13 01:54:39 +0800

    Basic login functionality

--- src/main/java/moe/ksmz/rodentraid/Api/Controller/AuthController.java
+++ src/main/java/moe/ksmz/rodentraid/Api/Controller/AuthController.java
@@ -1,30 +1,59 @@
 package moe.ksmz.rodentraid.Api.Controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import moe.ksmz.rodentraid.Auth.AuthStatus;
+import moe.ksmz.rodentraid.Auth.UserService;
 import moe.ksmz.rodentraid.Models.Repositories.UserRepository;
+import moe.ksmz.rodentraid.Request.Auth.Login;
 import moe.ksmz.rodentraid.Response.Auth.UserResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
     private final AuthStatus authStatus;
+    private final UserService userService;
     private final UserRepository userRepository;
 
     @GetMapping("/me")
     UserResponse me() {
         var user = authStatus.getCurrentUser().get();
 
-        return new UserResponse(user.getName(), user.getEmail(), user.getCurrentRank());
+        return UserResponse.fromUser(user);
     }
 
-    @GetMapping("/login")
-    String login() {
-        authStatus.setCurrentUser(userRepository.findById(1L).get());
-        return "ok";
+    @GetMapping("/forceLogin")
+    ResponseEntity<?> forceLogin() {
+        var user = userRepository.findById(1L);
+        if (user.isEmpty()) {
+            log.error("failed to force login id: {}", 1);
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        authStatus.setCurrentUser(user.get());
+
+        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+                .header(HttpHeaders.LOCATION, "/api/auth/me")
+                .build();
+    }
+
+    @PostMapping("/login")
+    ResponseEntity<UserResponse> login(@RequestBody Login login) {
+        var user = userService.attemptLogin(login.getEmail(), login.getPassword());
+
+        if (user.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        authStatus.setCurrentUser(user.get());
+
+        return ResponseEntity.ok(UserResponse.fromUser(user.get()));
     }
 }
