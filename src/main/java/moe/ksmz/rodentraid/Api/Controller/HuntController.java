05eea25be79f932e431d8e2e6efc8c2b94a3d62f
senkawa <mao@amatsuka.me>
2021-02-15 00:47:03 +0800

    Simple random rolls

--- src/main/java/moe/ksmz/rodentraid/Api/Controller/HuntController.java
+++ src/main/java/moe/ksmz/rodentraid/Api/Controller/HuntController.java
@@ -1,11 +1,15 @@
 package moe.ksmz.rodentraid.Api.Controller;
 
 import java.util.List;
+import java.util.Random;
+
 import moe.ksmz.rodentraid.Auth.AuthStatus;
 import moe.ksmz.rodentraid.Models.Hunt;
 import moe.ksmz.rodentraid.Models.Repositories.HuntRepository;
+import moe.ksmz.rodentraid.sck.Service.MiceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,19 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class HuntController {
     private AuthStatus authStatus;
     private HuntRepository huntRepository;
+    private MiceService miceService;
 
-    public HuntController(AuthStatus authStatus, HuntRepository huntRepository) {
+    public HuntController(AuthStatus authStatus, HuntRepository huntRepository, MiceService miceService) {
         this.authStatus = authStatus;
         this.huntRepository = huntRepository;
+        this.miceService = miceService;
     }
 
-    @GetMapping("/newHunt")
-    Hunt hunt() {
+    @GetMapping("/newHunt/{location}")
+    Hunt hunt(@PathVariable String location) {
         var u = authStatus.getCurrentUser();
+        var mice = miceService.allMiceForLocation(location);
+        var rand = new Random();
+        var randMouse = mice.get(rand.nextInt(mice.size()));
 
         var n = new Hunt();
-        n.setMouse("Alchemist");
-        n.setWeight(1L);
+        n.setMouse(randMouse.getName());
+        n.setWeight(randMouse.getWeight());
         n.setUser(u);
         huntRepository.save(n);
 
