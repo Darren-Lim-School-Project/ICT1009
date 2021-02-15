ade26841d26dcb1c35b19653154ba35ddcd1bbdb
senkawa <mao@amatsuka.me>
2021-02-15 17:04:54 +0800

    Move message to exception

--- src/main/java/moe/ksmz/rodentraid/Auth/UnauthorizedException.java
+++ src/main/java/moe/ksmz/rodentraid/Auth/UnauthorizedException.java
@@ -3,5 +3,9 @@ package moe.ksmz.rodentraid.Auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Not logged in")
-public class UnauthorizedException extends RuntimeException {}
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+    UnauthorizedException() {
+        super("Not logged in");
+    }
+}
