b67dd7d5126db466b075732db1c2a3f92a1a2d0b
senkawa <mao@amatsuka.me>
2021-02-22 00:56:48 +0800

    Use MockBeans instead

--- src/test/java/moe/ksmz/rodentraid/Feature/SmokeTest.java
+++ src/test/java/moe/ksmz/rodentraid/Feature/SmokeTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import moe.ksmz.rodentraid.Auth.AuthStatus;
+import moe.ksmz.rodentraid.Auth.PartyStatus;
 import moe.ksmz.rodentraid.Auth.UserService;
 import moe.ksmz.rodentraid.Models.Repositories.HuntRepository;
 import moe.ksmz.rodentraid.Models.Repositories.UserRepository;
@@ -11,27 +12,37 @@ import moe.ksmz.rodentraid.sck.Service.Contracts.HuntManager;
 import moe.ksmz.rodentraid.sck.Service.Contracts.LocationManager;
 import moe.ksmz.rodentraid.sck.Service.Contracts.MiceManager;
 import moe.ksmz.rodentraid.sck.Service.Contracts.TrapManager;
+import moe.ksmz.rodentraid.sck.Service.PartyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 @SpringJUnitWebConfig
+@MockBeans({
+        @MockBean(AuthStatus.class),
+        @MockBean(PartyStatus.class),
+        @MockBean(HuntRepository.class),
+        @MockBean(UserRepository.class),
+        @MockBean(UserService.class),
+        @MockBean(PartyService.class),
+        @MockBean(MiceManager.class),
+        @MockBean(HuntManager.class),
+        @MockBean(TrapManager.class),
+        @MockBean(LocationManager.class),
+        @MockBean(SimpMessagingTemplate.class)
+})
 public class SmokeTest {
-    @Autowired MockMvc http;
-    @Autowired MockHttpSession session;
-    @MockBean AuthStatus authStatus;
-    @MockBean HuntRepository huntRepository;
-    @MockBean UserRepository userRepository;
-    @MockBean UserService userService;
-    @MockBean MiceManager miceManager;
-    @MockBean HuntManager huntManager;
-    @MockBean TrapManager trapManager;
-    @MockBean LocationManager locationManager;
+    @Autowired
+    MockMvc http;
+    @Autowired
+    MockHttpSession session;
 
     @Test
     void itWorks() throws Exception {
